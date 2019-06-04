8260bdd486aacca0eea5413eb27885e28d212105
unknown <dwightanthony@live.com>
2019-06-04 15:31:04 -0400

    Addition stuff

--- src/main/java/com/revature/controllers/XMLController.java
+++ src/main/java/com/revature/controllers/XMLController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,10 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.revature.models.Pom;
+import com.revature.services.FileService;
 
 @RestController
 @RequestMapping("/xml")
 public class XMLController {
+	
+	FileService fileService = new FileService();
 	
 	@PostMapping("")
 	public void runXMLParser(@RequestBody String url) {
@@ -29,4 +33,12 @@ public class XMLController {
 			e.printStackTrace();
 		}
 	}
+	
+	
+	@GetMapping("/getfile")
+	public void getFile(@RequestBody String filePath) {
+		fileService.FindFile(filePath);
+		
+	}
+	
 }
