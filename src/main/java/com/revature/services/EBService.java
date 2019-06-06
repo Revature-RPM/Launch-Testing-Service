1be996d95762e341cdb96b2626d372c6f0523583
DevinStuart <onefunnystuart@gmail.com>
2019-06-06 11:24:42 -0400

    Attempting to create application

--- src/main/java/com/revature/services/EBService.java
+++ src/main/java/com/revature/services/EBService.java
@@ -3,6 +3,7 @@ package com.revature.services;
 import org.springframework.stereotype.Service;
 
 import software.amazon.awssdk.services.elasticbeanstalk.ElasticBeanstalkClient;
+import software.amazon.awssdk.services.elasticbeanstalk.model.CreateApplicationVersionRequest;
 import software.amazon.awssdk.services.elasticbeanstalk.model.CreateEnvironmentRequest;
 import software.amazon.awssdk.services.elasticbeanstalk.model.CreateEnvironmentResponse;
 import software.amazon.awssdk.services.elasticbeanstalk.model.S3Location;
@@ -17,22 +18,23 @@ public class EBService {
 		// String s3ARN = "arn:aws:s3:::expenseapplication";
 		//String s3Key = "It's an s3 key!!!!";
 		String versionLabel = "label1.1";
+		String objectURL = "https://norobotsplzdontfindme.s3.amazonaws.com/spring-boot.jar";
 		String name = "Tom is cool and so is devin";
 		String stackName = "64bit Amazon Linux 2018.03 v2.8.3 running Java 8";
 		
 		S3Location s3Bucket = S3Location.builder()
 				.s3Bucket(s3)
-				.s3Key("")
+				.s3Key("spring-boot.jar")
 				.build();
 		ElasticBeanstalkClient bean = ElasticBeanstalkClient.create();
-		//CreateApplicationRequest 
+		//CreateApplicationRequest
+		
 		// Generate and attach Procfile
-		/*CreateApplicationVersionRequest applicationRequest = CreateApplicationVersionRequest.builder()
-				//.sourceBundle(s3Bucket)
-				.autoCreateApplication(true)
-				.versionLabel(versionLabel)
+		CreateApplicationVersionRequest applicationRequest = CreateApplicationVersionRequest.builder()
+				.sourceBundle(s3Bucket)
 				.applicationName(name)
-				.build();*/
+				.versionLabel(versionLabel)
+				.build();
 		CreateEnvironmentRequest envRequest = CreateEnvironmentRequest.builder()
 				.applicationName(name)
 				.versionLabel(versionLabel)
