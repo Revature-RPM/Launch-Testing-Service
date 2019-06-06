bdd566932f102a09d5db6fc9d6b627d524341c52
DevinStuart <onefunnystuart@gmail.com>
2019-06-06 11:07:33 -0400

    commenting out s3bucketARN

--- src/main/java/com/revature/services/EBService.java
+++ src/main/java/com/revature/services/EBService.java
@@ -13,8 +13,8 @@ import software.amazon.awssdk.services.elasticbeanstalk.model.TerminateEnvironme
 public class EBService {
 
 	public String CreateEC2() {
-		String s3 = "expenseapplication"; // These need to be passed from somewhere
-		String s3ARN = "arn:aws:s3:::expenseapplication";
+		String s3 = "norobotsplzdontfindme"; // These need to be passed from somewhere
+		// String s3ARN = "arn:aws:s3:::expenseapplication";
 		//String s3Key = "It's an s3 key!!!!";
 		String versionLabel = "label1.1";
 		String name = "Tom is cool and so is devin";
@@ -22,7 +22,7 @@ public class EBService {
 		
 		S3Location s3Bucket = S3Location.builder()
 				.s3Bucket(s3)
-				.s3Key(s3Key)
+				.s3Key("")
 				.build();
 		ElasticBeanstalkClient bean = ElasticBeanstalkClient.create();
 		//CreateApplicationRequest 
