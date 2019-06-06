cdc900292a3cfc12f90b3b4a625ad2ebab7a137a
Thomas Kirby <tkirby0322@gmail.com>
2019-06-06 09:39:55 -0400

    added docs to RDS creation

--- src/main/java/com/revature/services/RDSService.java
+++ src/main/java/com/revature/services/RDSService.java
@@ -8,21 +8,25 @@ import software.amazon.awssdk.services.rds.model.CreateDbInstanceRequest;
 @Service
 public class RDSService {
 	
+	/**
+	 * Provisioning an AWS RDS instance
+	 * @return the AWS response string showing all spun up server stats
+	 */
 	public String CreateRds() {
 		//RdsClient rds = new Client();
-		String instanceClass = "db.t2.micro";
-		String instanceIdentifier = "devinandtomarecool";
-		String engine = "postgres";
-		RdsClient rds = RdsClient.create();
-		CreateDbInstanceRequest createDbInstanceRequest = CreateDbInstanceRequest.builder()
+		String instanceClass = "db.t2.micro";//required for free tier
+		String instanceIdentifier = "devinandtomarecool";//name the RDS instance
+		String engine = "postgres";//engine type (postgres/SQL/MySQL etc)
+		RdsClient rds = RdsClient.create(); //create an empty client used for talking to AWS RDS SDK
+		CreateDbInstanceRequest createDbInstanceRequest = CreateDbInstanceRequest.builder()//start creating the request
 				.dbInstanceClass(instanceClass)
 				.dbInstanceIdentifier(instanceIdentifier)
 				.engine(engine)
-				.masterUsername("dwightbrown")
-				.masterUserPassword(System.getenv("JDBC_PASSWORD"))
-				.allocatedStorage(20)
-				.build();
-		String response = rds.createDBInstance(createDbInstanceRequest).toString();
+				.masterUsername("dwightbrown")//admin username for the Username
+				.masterUserPassword(System.getenv("JDBC_PASSWORD"))//admin password
+				.allocatedStorage(20)//number of storage in GB
+				.build();//formally build request
+		String response = rds.createDBInstance(createDbInstanceRequest).toString();//send provision request get provision response
 		return response;
 	}
 }
